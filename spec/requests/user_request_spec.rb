46ad201cc68a4215fc5e00949ed701b135ba0f97
Horecio Araujo Dias <horecio@gmail.com>
2025-05-20 02:15:45 +0000

    fix(user): change scope by_username

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  let!(:company_1) { create(:company) }
+  let!(:company_2) { create(:company) }
 
   RSpec.shared_context 'with multiple companies' do
-    let!(:company_1) { create(:company) }
-    let!(:company_2) { create(:company) }
-
     before do
       5.times do
         create(:user, company: company_1)
@@ -24,9 +23,24 @@ RSpec.describe "Users", type: :request do
 
       it 'returns only the users for the specified company' do
         get company_users_path(company_1)
-        
+
         expect(result.size).to eq(company_1.users.size)
         expect(result.map { |element| element['id'] } ).to eq(company_1.users.ids)
+      end
+    end
+
+    context 'when fetching user by username' do
+      before do
+        create(:user, username: 'Max', company: company_1)
+        create(:user, username: 'Mathew', company: company_1)
+        create(:user, username: 'John', company: company_2)
+        create(:user, username: 'Jane', company: company_2)
+      end
+
+      it 'returns all the users' do
+        get company_users_path(company_1, username: 'ma')
+
+        expect(result.size).to eq(2)
       end
     end
 
